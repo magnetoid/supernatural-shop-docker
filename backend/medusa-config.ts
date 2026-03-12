b92ea2f8eccd2b6bd347621caafed004c55b50b4
root <root@tetra.imbamarketing.com>
2026-03-12 17:22:37 +0100

    fix: dodaj backendUrl u admin config

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,7 +1,5 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
-
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -12,5 +10,8 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    backendUrl: "https://backend.supernatural-shop.com",
   }
 })
